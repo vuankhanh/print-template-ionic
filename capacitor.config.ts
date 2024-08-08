356e36f7223cef1e077bb0aebb6db10d0738aa00
VU AN KHANH <vuankhanh19071992@gmail.com>
2024-08-08 14:25:40 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'print-template-ionic',
+  webDir: 'www'
+};
+
+export default config;
